a8fa77bb192c7a70c7d1765f5373211414879988
EQuimper <quimperemanuel@gmail.com>
2017-05-18 18:25:02 -0400

    Finish ep-03

---
+++ src/modules/users/user.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from 'mongoose';
+import validator from 'validator';
+
+import { passwordReg } from './user.validations';
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Email is required!'],
+    trim: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+      message: '{VALUE} is not a valid email!',
+    },
+  },
+  firstName: {
+    type: String,
+    required: [true, 'FirstName is required!'],
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'LastName is required!'],
+    trim: true,
+  },
+  userName: {
+    type: String,
+    required: [true, 'UserName is required!'],
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required!'],
+    trim: true,
+    minlength: [6, 'Password need to be longer!'],
+    validate: {
+      validator(password) {
+        return passwordReg.test(password);
+      },
+      message: '{VALUE} is not a valid password!',
+    },
+  },
+});
+
+export default mongoose.model('User', UserSchema);
